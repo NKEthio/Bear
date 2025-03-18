b4149cb9d6ec2bf8c7013e87b70e4ea035a2ddcf
NKEthio <nuredinkassaw599@gmail.com>
2025-03-18 14:22:13 +0300

    Landing-page Updated

--- src/pages/AmHome.jsx
+++ src/pages/AmHome.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom"
+import "./AmHome.css";
 
 export default function AmHome() {
 
     return(
-        <div lang="am">
+        <div lang="am" className="am-home-container">
             <h1>የአማርኛ በይነ መረብ የቤት አቅጣጫ</h1>
+            <Link to="/hahu">
+                <img src="./Hahu.jpg" alt="ፊደላት" />
+                <br />
+                <button >ፊደላት</button>
+            </Link>
         </div>
     )
 }
